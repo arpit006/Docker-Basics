5d87a6547ae1544952aedff58755c5bf7006d504
Arpit Srivastava <iarpitsrivastava06@gmail.com>
2020-01-18 13:50:58 +0530

    Jokes app image changes for Rolling Update

--- Jokes-Spring-Boot-App/src/main/java/com/springboot/jokesapplication/controllers/JokeController.java
+++ Jokes-Spring-Boot-App/src/main/java/com/springboot/jokesapplication/controllers/JokeController.java
@@ -6,18 +6,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 @Controller
 public class JokeController {
 
     private JokesService jokesService;
 
+    private AtomicInteger atomicInteger;
+
     @Autowired
     public JokeController(JokesService jokesService) {
         this.jokesService = jokesService;
+        atomicInteger = new AtomicInteger();
     }
 
     @RequestMapping({"/", ""})
     public String showJoke(Model model) {
+        System.out.println("Get Request called for " + atomicInteger.addAndGet(1) + " times.");
         model.addAttribute("joke", jokesService.getJoke());
         return "chucknorris";
     }
